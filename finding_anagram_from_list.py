ac44fd64d5c1e725f5ed1df3be4a8786c3302dd6
TP2512 <119523299+TP2512@users.noreply.github.com>
2022-12-13 16:08:41 +0530

    finding_anagrams_in_list

---
+++ finding_anagram_from_list.py
@@ -0,0 +1,21 @@
+def anagram(str1,str2):
+  if sorted(str1)==sorted(str2):
+    return True
+
+def checklist(clist):
+  List2=[]
+  n=len(clist)
+  for i in range(n):
+    str1=List1[i]
+    k=i+1
+    for j in range(n-1):
+      ei=k%n
+      str2=clist[ei]
+      if anagram(str1,str2):
+          List2.append(str1)
+          break
+      k+=1
+  return List2
+
+List1 = ["pool", "hey", "why", "polo", "pol", "lopo", "apolo", "hye"]
+print(checklist(List1))
